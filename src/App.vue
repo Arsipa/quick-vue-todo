785a3ac5fb31fdd9a40936e18a050f69d8668c4a
arsipa <arsipa.dev@gmail.com>
2024-02-19 22:38:55 +0300

    todo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="wrapper">
+  <h1>TODO</h1>
+  <button class="create" @click="createTask">create task</button>
+  <ul>
+    <listItem 
+      v-for="(task, index) in tasks" 
+      :task="task"
+      :key="index" 
+      @apply="(newValue) => updateTask(index, newValue)" 
+      @delete="deleteTask(index)"
+    />
+  </ul>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import listItem from './components/listItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    listItem
+  },
+  data() {
+    return {
+      tasks: [
+        "Покакать",
+        "Пописить",
+        "Анжумания",
+        "Пресс качат",
+        "Бегит",
+      ],
+    }
+  },
+  methods: {
+    updateTask(index, newValue) {
+      this.tasks[index] = newValue;
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    createTask() {
+      this.tasks.push(prompt('task name'))
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 50px 0;
+}
+.create {
+  padding: 20px;
+  font-size: 20px;
+  color: white;
+  background: blue;
+  border-radius: 10px;
+}
+ul {
+  padding: 30px 0;
 }
 </style>
---
+++ src/components/listItem.vue
@@ -0,0 +1,67 @@
+<template>
+  <li>
+    <template v-if="isEditing">
+      <input type="text" v-model="newValue">
+      <button @click="onApplyClick">apply</button>
+    </template>
+    <template v-else>
+      {{task}}
+      <button @click="isEditing=true">edit</button>
+      <button class="delete" @click="$emit('delete')">delete</button>
+    </template>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    task: String
+  },
+  data() {
+    return {
+      isEditing: false,
+      newValue: this.task,
+    }
+  },
+  methods: {
+    onApplyClick() {
+      this.$emit('apply', this.newValue);
+      this.isEditing = false;
+    }
+  }
+}
+</script>
+
+<style scoped>
+li {
+  width: 300px;
+  color: black;
+  font-size: 30px;
+  cursor: pointer;
+  border: 1px solid black;
+  padding: 20px;
+  margin: 10px;
+  border-radius: 10px;
+  text-align: center;
+}
+input {
+  display: block;
+  width: 100%;
+  font-size: 20px;
+  text-align: center;
+}
+button {
+  display: block;
+  background: green;
+  border-radius: 5px;
+  width: 100%;
+  margin: 10px 0;
+  padding: 2px;
+  cursor: pointer;
+  color: white;
+}
+.delete {
+  background: red;
+}
+</style>
